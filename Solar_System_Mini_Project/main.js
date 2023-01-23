3c1ff19ed64234a70e76346639568026315b8364
Rubxn <rubenbm198@gmail.com>
2023-01-23 19:01:35 +0100

    Finished

--- Solar_System_Mini_Project/main.js
+++ Solar_System_Mini_Project/main.js
@@ -19,11 +19,78 @@ for(var planet in planets) {
     select.appendChild(opt)
 }
 
+const image = document.getElementById('planet-image')
+let planetSelected = 'none'
 var select = document.getElementById("select")
 select.addEventListener('change', (event) => {
-    const image = document.getElementById('planet-image')
     image.src = ''
     if(event.target.value != 'none'){
+        planetSelected = event.target.value
         image.src = `./images/${event.target.value}.png`
     }
 })
+
+
+const result = `<div class="result"> 
+                    <p>${4}</p>
+                </div>`
+
+const mass = document.getElementById('mass')
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', e => {
+    console.log('click')
+    const flexContainer = document.querySelector('.flex-container')
+    cleanDescripion(flexContainer)
+    if(mass.value === '') {
+        const div = createDescription('Mass is required', false)
+        flexContainer.appendChild(div)
+
+    }
+     if(planetSelected ==='none'){
+        image.src = ''
+        if(mass.value !== ''){
+            const div = createDescription('You did not choose a planet yet', false)
+            flexContainer.appendChild(div)
+        }
+        
+    } 
+    if(mass.value !== '' && planetSelected !=='none') {
+        const div = createDescription(`The weight of the object on ${planetSelected}`, true)
+        flexContainer.appendChild(div)
+    }
+    
+})
+
+function createDescription(text, correct) {
+    let div = document.createElement('div')
+    div.className = 'flex-item description'
+    div.id = 'description'
+    let p = document.createElement('p')
+    p.appendChild(document.createTextNode(text))
+    div.appendChild(p)
+
+    if(correct) {
+        let res = document.createElement('div')
+        res.className = 'result'
+        res.appendChild(document.createTextNode(`${calculate()} N`))
+        div.appendChild(res)
+    }
+
+    return div
+}
+
+function cleanDescripion(flexContainer){
+    const description = document.getElementById('description')
+
+    if(description !== null){
+        flexContainer.removeChild(description)
+    }
+
+}
+
+
+function calculate() {
+    return ((mass.value / 9.81) * planets[planetSelected]).toFixed(2)
+}
